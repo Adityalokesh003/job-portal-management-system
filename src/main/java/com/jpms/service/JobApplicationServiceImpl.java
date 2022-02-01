2d38af74bea10c6ccf2269a65819ca00ccdebcb0
Adityalokesh003 <adityalokesh003@gmail.com>
2022-02-01 17:28:17 +0530

    adding readme file

--- src/main/java/com/jpms/service/JobApplicationServiceImpl.java
+++ src/main/java/com/jpms/service/JobApplicationServiceImpl.java
@@ -3,10 +3,12 @@ package com.jpms.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.jpms.dao.JobApplicationRepository;
 import com.jpms.entity.JobApplication;
 
+@Service
 public class JobApplicationServiceImpl implements JobApplicationService {
 	@Autowired
 	private JobApplicationRepository jobApplicationRepository;
